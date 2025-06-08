3fc9db8a94a0b792d5e796056a11b4d88c0a01fb
Vrutti Patel <vruttiptl2015@gmail.com>
2025-06-08 23:36:35 +0530

    dashboard done

--- TMS/login_handler.php
+++ TMS/login_handler.php
@@ -19,7 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['role'] = $row['role'];
 
             // Redirect based on role
-                    header("Location: user_dashboard.php");
             header("Location: dashboard.php");
             exit();
         } else {
